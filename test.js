d2249ab73e3b3dbda73c403854ca90c374aeedd4
Linus Unnebäck <linus@folkdatorn.se>
2019-04-05 12:05:06 +0100

    💥 Require Node.js >= 6.x
    Migration Guide:
    
    Upgrade to Node.js 6.x or later.

--- test.js
+++ test.js
@@ -6,12 +6,12 @@ const loadYamlFile = require('./')
 describe('load-yaml-file', () => {
   it('loadYamlFile()', () => {
     return loadYamlFile('foo.yml').then(data => {
-      assert.deepEqual(data, { foo: true })
+      assert.deepStrictEqual(data, { foo: true })
     })
   })
 
   it('loadYamlFile.sync()', () => {
     const data = loadYamlFile.sync('foo.yml')
-    assert.deepEqual(data, { foo: true })
+    assert.deepStrictEqual(data, { foo: true })
   })
 })
